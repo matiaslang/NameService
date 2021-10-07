fdc40d8726c6308beb73c3b3ad2d80427a851e09
Matias Lang <matiaslang@icloud.com>
2021-10-07 22:09:34 +0300

    Initial commit

---
+++ src/NameSorterProcessor/LambdaEntryPoint.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace NameSorterProcessor
+{
+    /// <summary>
+    /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
+    /// actual Lambda function entry point. The Lambda handler field should be set to
+    /// 
+    /// NameSorterProcessor::NameSorterProcessor.LambdaEntryPoint::FunctionHandlerAsync
+    /// </summary>
+    [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        /// <summary>
+        /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
+        /// needs to be configured in this method using the UseStartup<>() method.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+
+        /// <summary>
+        /// Use this override to customize the services registered with the IHostBuilder. 
+        /// 
+        /// It is recommended not to call ConfigureWebHostDefaults to configure the IWebHostBuilder inside this method.
+        /// Instead customize the IWebHostBuilder in the Init(IWebHostBuilder) overload.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IHostBuilder builder)
+        {
+        }
+    }
+}
